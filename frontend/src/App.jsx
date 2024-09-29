f32bd45417b333453612c7adac73e5b5c6709942
mugeishkumar912005 <kmugeis2005@gmail.com>
2024-09-29 09:40:50 +0530

    mk 6th commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,16 @@ import Userpage from './components/Userpage/Userpage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginSignup from './components/LoginSignup/LoginSignup';
 import { AuthProvider } from './AuthContext';
-import BookingPage from './components/BookingPage';
+import BookingPage from './components/BookingPage/BookingPage.jsx';
 import Cart from './components/Cart/Cart';
 import Dashboard from './components/dashboard';
+import { ToastContainer } from 'react-toastify';
 
 const App = () => {
   return (
 <AuthProvider>
    <Router>
+    <ToastContainer/>
     <Routes>
       <Route path="/" element={<Userpage/>}/>
       <Route path="/login" element={<LoginSignup/>}/>
